91500419aabbcb7e8d17d1a5d7b3f18a612f81b1
Shohboz Xurramov <shohboztuit@gmail.com>
2023-10-04 19:15:55 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "jeck", "Jako", "Kol9oi" };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (ValidName(names[i]))
+                {
+                    Console.WriteLine("okeyyyyyyyyyy");
+                }
+                else
+                {
+                    Console.WriteLine("nooooooooooo");
+                }
+            }
+
+
+        }
+        static bool ValidName(string names)
+        {
+            int count = 0;
+           
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (char.IsLetter(names[i]))
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+            return count==names.Length && char.IsUpper(names[0]);
+
+        }
+    }
+}
